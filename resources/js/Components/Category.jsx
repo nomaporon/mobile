c7d0f966052bcf350221191c73f3d0fd9e2897d7
nomaporon <ono.5yuta.2masa.9@gmail.com>
2023-09-07 14:18:11 +0900

    カテゴリごとにメニュー一覧を変更する昨日の追加

--- resources/js/Components/Category.jsx
+++ resources/js/Components/Category.jsx
@@ -1,33 +1,15 @@
 import React, { useState } from 'react';
-import Food from './Food';
 
 const Category = (props) => {
-    const category = props.category_item
-    console.log(category)
+    const category_name = props.category_name
     const [isClickCategory, handleClickCategory] = useState(false);
-    let category_index;
-    if (isClickCategory) {
-      category_index = (
-        <div className="category-index">
-          { category.foods.map((food) => (
-              <div className="food">
-                  <Food
-                      food_name={food.name} 
-                      food_price={food.price}
-                  />
-              </div>
-          ) )}
-        </div>
-        );
-    }
     
     return (
         <div
           className='category-item'
           onClick={() => {handleClickCategory(true)}}
         >
-          <p>{category.category}</p>
-          {category_index}
+          <p>{category_name}</p>
         </div>
         );
 }
